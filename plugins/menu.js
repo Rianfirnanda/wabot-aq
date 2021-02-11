e9430c91d337ca140b450094cde47ac427fd3eba
Rianfirnanda <69239761+Rianfirnanda@users.noreply.github.com>
2021-02-11 09:20:42 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -74,25 +74,26 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     conn.menu = conn.menu ? conn.menu : {}
     let before = conn.menu.before || `
 ╭─「 ${conn.getName(conn.user.jid)} 」
-│ Hai, %name!
+│ Hai kak, %name!
 │
 │ *%exp XP*
 │ Tersisa *%limit Limit*
 │
-│ Tanggal: *%week %weton, %date*
+│ Tanggal: *%week , %date*
 │ Waktu: *%time*
 │
-│ Uptime: *%uptime*
-│ Main Uptime: *%muptime*
-│ Database: %totalreg nomor
-│ Github:
-│ %github
+│ Waktu Aktif: *%uptime*
+│ Total: *%muptime*
+│ Pengguna: %totalreg nomor
+│
+│ Owner bot
+│ ig: https://bit.ly/3752Xsm
 ╰────
 %readmore`
     let header = conn.menu.header || '╭─「 %category 」'
     let body   = conn.menu.body   || '│ • %cmd%islimit'
     let footer = conn.menu.footer || '╰────\n'
-    let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
+    let after  = conn.menu.after  || (conn.user.jid == global.conn.user.jid ? '' : `Di Tenagai oleh https://wa.me/${global.conn.user.jid.split`@`[0]}`) + `\n*%npmname@^%version*\n\`\`\`\%npmdesc\`\`\``
     let _text  = before + '\n'
     for (let tag in groups) {
       _text += header.replace(/%category/g, tags[tag]) + '\n'
