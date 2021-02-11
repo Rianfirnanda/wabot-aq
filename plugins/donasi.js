6b1f71adabf8de141fb1fd5091778a86f39da793
Rianfirnanda <69239761+Rianfirnanda@users.noreply.github.com>
2021-02-11 09:17:13 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • TRI [089606032177]
+│ • Dana [089606032177]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
