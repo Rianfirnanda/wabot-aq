3a6ed173a7b2ac2023e143577b9e22f65078d0a1
Rianfirnanda <69239761+Rianfirnanda@users.noreply.github.com>
2021-02-11 09:19:04 +0700

    Update leaderboard.js

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -5,12 +5,12 @@ let handler = async (m, { conn, args }) => {
   let usersLim = sortedLim.map(v => v[0])
   let len = args[0] && args[0].length > 0 ? Math.min(1000, Math.max(parseInt(args[0]), 5)) : Math.min(20, sortedExp.length)
   let text = `
-• *XP Leaderboard Top ${len}* •
+• *Rank TOP XP Saat ini ${len}* •
 Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
 
 ${sortedExp.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.exp + ' Exp*').join`\n`}
 
-• *Limit Leaderboard Top ${len}* •
+• *Rank Limit Terbanyak Saat ini ${len}* •
 Kamu: *${usersLim.indexOf(m.sender) + 1}* dari *${usersLim.length}*
 
 ${sortedLim.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.limit + ' Limit*').join`\n`}
